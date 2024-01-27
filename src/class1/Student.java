d346bf0e8950bb22d7b557b7d66eb96940355489
ohsang14 <ast6108@naver.com>
2024-01-27 16:12:20 +0900

    1월 27일

---
+++ src/class1/Student.java
@@ -0,0 +1,7 @@
+package class1;
+
+public class Student { // 설계도
+    String name;
+    int age;
+    int grade;
+}
--- src/test.java
+++
@@ -1,5 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println(" 나 한번 테스트 해보리오!");
-    }
-}
