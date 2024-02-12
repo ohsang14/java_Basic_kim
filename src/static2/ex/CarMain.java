3938ff167247a59c28eaa9afbf10c39cbbcccd62
ohsang14 <ast6108@naver.com>
2024-02-12 19:21:54 +0900

    2월 12일/ static 문제 풀이( 개어려움 )

---
+++ src/static2/ex/CarMain.java
@@ -0,0 +1,12 @@
+package static2.ex;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car1 = new Car("k3");
+        Car car2 = new Car("G80");
+        Car car3 = new Car("modle y");
+
+        Car.showTotalCars();
+
+    }
+}
