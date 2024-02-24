f6e0b759970aae02b7c8fb325792b06e49ed67eb
ohsang14 <ast6108@naver.com>
2024-02-24 23:23:14 +0900

    2월 24일/ 상속, Override

--- src/final1/ex/Member.java
+++ src/final1/ex/Member.java
@@ -1,19 +1,17 @@
 package final1.ex;
 
 public class Member {
-    private final String id; // final 키워드 사용
+    private final String id;
     private String name;
-
     public Member(String id, String name) {
         this.id = id;
         this.name = name;
     }
-
-    public void changeData(String id, String name) {
-        // this.id = id; // 컴파일 오류
+    public void changeData(String id, String name){
+        // this.id = id;
         this.name = name;
     }
     public void print(){
-        System.out.println("id: " + id + ", name: " + name);
+        System.out.println("id:" + id + ", name" + name);
     }
 }
